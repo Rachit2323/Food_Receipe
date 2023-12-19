be36b4500939bb47943135571b18bd5ee6be5844
Rachit2323 <76684704+Rachit2323@users.noreply.github.com>
2023-12-20 01:41:37 +0530

    routes_done

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,46 @@
-
 import './App.css';
 import Dash from './components/Dash/Dash.js';
-
 import RecipeDetail from './components/Dash/ReceipeDetails.js';
 import Signup from './components/Signup/Signup.js';
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+
+  const isAuthenticated = localStorage.getItem('token');
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/dash" element={<Dash />} />
+          {/* Public Route - Sign Up */}
+          <Route
+            path="/"
+            element={
+              isAuthenticated ? (
+                <Navigate to="/dash" />
+              ) : (
+                <Signup />
+              )
+            }
+          />
 
-          <Route path="/add/:id" element={<RecipeDetail edit={false}/>} />
-          <Route path="/edit/:id" element={<RecipeDetail edit={true}/>} />
+          {/* Private Route - Dashboard */}
+          <Route
+            path="/dash"
+            element={isAuthenticated ? <Dash /> : <Navigate to="/" />}
+          />
 
-          
-          
+          {/* Private Route - Add Recipe */}
+          <Route
+            path="/add/:id"
+            element={isAuthenticated ? <RecipeDetail edit={false} /> : <Navigate to="/" />}
+          />
+
+          {/* Private Route - Edit Recipe */}
+          <Route
+            path="/edit/:id"
+            element={isAuthenticated ? <RecipeDetail edit={true} /> : <Navigate to="/" />}
+          />
         </Routes>
       </div>
     </Router>
